14bbe644edf92df2fb131d5e406d3a4eb8b172f9
Lennart <Lennartmeeuse@gmail.com>
2026-02-24 16:22:21 +0100

    1.0.37  - small query change in GetAllDistinctBewerkingen.sql

--- Data/Query's/GetAllDistinctBewerkingen.sql
+++ Data/Query's/GetAllDistinctBewerkingen.sql
@@ -1,7 +1,9 @@
+  USE [ProduktieDB]
+  
   SELECT MG.MachGrpCode
  
   FROM   dbo.T_MachGrp MG  
   WHERE  MG.MachGrpCode > N''  
   AND MG.DeptCode IN ('BAFR','DRAA','EX','FREZ','MA01','MA02','ZG','TEK2')
-  AND MG.MachGrpCode NOT IN ('PLM','SGVK','VER')
+  AND MG.MachGrpCode NOT IN ('PLM')
   ORDER BY 1  
